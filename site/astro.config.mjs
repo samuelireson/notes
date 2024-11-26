77dc7e7fba407e4311502394eba969011c9c2162
Samuel Ireson <samuelireson@gmail.com>
2024-11-26 12:13:43 +0000

    skeleton

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -9,7 +9,8 @@ export default defineConfig({
 	site: 'https://samuelireson.github.io',
 	base: '/notes',
 	integrations: [starlight({
-		title: 'notes',
+		title: '✍ notes',
+		favicon: '/favicon.ico',
 		social: {
 			github: 'https://github.com/samuelireson/notes'
 		},
@@ -19,6 +20,16 @@ export default defineConfig({
 				items: [
 					{ label: 'Topological spaces', slug: 'introduction-to-topological-manifolds/chapters/topological-spaces' },
 					{ label: 'New spaces from old', slug: 'introduction-to-topological-manifolds/chapters/new-spaces-from-old' },
+					{ label: 'Connectedness and compactness', slug: 'introduction-to-topological-manifolds/chapters/connectedness-and-compactness' },
+					{ label: 'Cell complexes', slug: 'introduction-to-topological-manifolds/chapters/cell-complexes' },
+					{ label: 'Compact surfaces', slug: 'introduction-to-topological-manifolds/chapters/compact-surfaces' },
+					{ label: 'Homotopy and the fundamental group', slug: 'introduction-to-topological-manifolds/chapters/homotopy-and-the-fundamental-group' },
+					{ label: 'The circle', slug: 'introduction-to-topological-manifolds/chapters/the-circle' },
+					{ label: 'Some group theory', slug: 'introduction-to-topological-manifolds/chapters/some-group-theory' },
+					{ label: 'The Seifert-Van Kampen theorem', slug: 'introduction-to-topological-manifolds/chapters/seifert-van-kampen' },
+					{ label: 'Covering maps', slug: 'introduction-to-topological-manifolds/chapters/covering-maps' },
+					{ label: 'Group actions and covering maps', slug: 'introduction-to-topological-manifolds/chapters/group-actions-and-covering-maps' },
+					{ label: 'Homology', slug: 'introduction-to-topological-manifolds/chapters/homology' },
 				],
 			},
 		],
@@ -30,6 +41,16 @@ export default defineConfig({
 	})],
 	markdown: {
 		remarkPlugins: [remarkMath],
-		rehypePlugins: [rehypeKatex],
+		rehypePlugins: [
+			[rehypeKatex, {
+				macros: {
+					"\\boundary": "\\partial\\!",
+					"\\exterior": "\\mathrm{Ext}",
+					"\\homeomorphic": "\\approx",
+					"\\interior": "\\mathrm{Int}",
+					"\\identity": "\\mathrm{Id}",
+				}
+			}]
+		],
 	}
 });
